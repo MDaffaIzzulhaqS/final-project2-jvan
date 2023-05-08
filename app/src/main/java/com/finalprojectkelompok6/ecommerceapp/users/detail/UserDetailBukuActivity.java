c34ccaefaaed5d9cbc54a4d01eb2cdb140b5c5e1
fnsyhslsbla <fanisyahsalsabila20@gmail.com>
2023-05-08 17:59:54 +0700

    update layout

--- app/src/main/java/com/finalprojectkelompok6/ecommerceapp/users/detail/UserDetailBukuActivity.java
+++ app/src/main/java/com/finalprojectkelompok6/ecommerceapp/users/detail/UserDetailBukuActivity.java
@@ -3,14 +3,25 @@ package com.finalprojectkelompok6.ecommerceapp.users.detail;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.finalprojectkelompok6.ecommerceapp.R;
 
 public class UserDetailBukuActivity extends AppCompatActivity {
+    private ImageView bukuImg;
+    private TextView bukuTittle;
+    private TextView bukuStock;
+    private TextView bukuPrice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_detail_buku);
+
+        bukuImg = findViewById(R.id.img_buku);
+        bukuTittle = findViewById(R.id.tittle_buku);
+        bukuStock = findViewById(R.id.stock_buku);
+        bukuPrice = findViewById(R.id.price_buku);
     }
 }
--- app/src/main/java/com/finalprojectkelompok6/ecommerceapp/users/detail/UserDetailKueActivity.java
+++ app/src/main/java/com/finalprojectkelompok6/ecommerceapp/users/detail/UserDetailKueActivity.java
@@ -3,14 +3,27 @@ package com.finalprojectkelompok6.ecommerceapp.users.detail;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.finalprojectkelompok6.ecommerceapp.R;
 
 public class UserDetailKueActivity extends AppCompatActivity {
+    private ImageView kueImg;
+    private TextView kueTittle;
+    private TextView kueStock;
+    private TextView kuePrice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_detail_kue);
+
+        kueImg = findViewById(R.id.img_kue);
+        kueTittle = findViewById(R.id.tittle_kue);
+        kueStock = findViewById(R.id.stock_kue);
+        kuePrice = findViewById(R.id.price_kue);
+
+
     }
 }
